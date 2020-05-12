78ff1e9d27b22a238986b1e31fa7f91ee00d6160
Lorenz <lorenz.hartwig94@gmail.com>
2020-05-12 10:05:34 +0200

    IntoleranceFragment

--- app/src/main/java/com/example/navigationdrawerfromscratch/IntoleranceFragment.java
+++ app/src/main/java/com/example/navigationdrawerfromscratch/IntoleranceFragment.java
@@ -1,30 +1,58 @@
 package com.example.navigationdrawerfromscratch;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.SearchView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class IntoleranceFragment extends AppCompatActivity {
+import com.example.navigationdrawerfromscratch.lebensmittel.Gemüse;
+
+public class IntoleranceFragment extends Fragment {
 
 
     @Nullable
 
-    private EditText mSearchField;
+    private SearchView mSearchField;
     private RecyclerView mResultList;
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    @Nullable
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_create_account, container, false);
+
+        mSearchField = (SearchView) view.findViewById(R.id.search_field);
+        mResultList = (RecyclerView) view.findViewById(R.id.intolerance_list);
 
 
-        mSearchField = (EditText) findViewById(R.id.search_field);
 
-        mResultList = (RecyclerView) findViewById(R.id.intolerance_list);
+        return view;
+    }
+
+
+//View Holder Class
+
+    public class FoodViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        public FoodViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mView = itemView;
+
+        }
 
 
     }
+
+
 }
