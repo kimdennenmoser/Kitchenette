d82d4624aeb05bfe5602b1d5194c84fd09649ae6
kiim0801 <kim.dennenmoser@googlemail.com>
2020-05-13 13:02:08 +0200

    Login Anfänge

--- app/src/main/java/com/example/navigationdrawerfromscratch/AccountFragment.java
+++ app/src/main/java/com/example/navigationdrawerfromscratch/AccountFragment.java
@@ -1,26 +1,48 @@
 package com.example.navigationdrawerfromscratch;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccountFragment extends Fragment {
+
+    View view;
+    TextView textView;
+    Button anmeldenBtn;
+    EditText editTextInsertUsername;
+    String editTextInsertUsernameString;
+    DatabaseReference databaseUser;
+    Context context;
+
+    List<User> userList;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-
-        View view = inflater.inflate(R.layout.fragment_account, container, false);
-        TextView textView = (TextView) view.findViewById(R.id.textViewCreateAccount);
+        view = inflater.inflate(R.layout.fragment_account, container, false);
+        textView = (TextView) view.findViewById(R.id.textViewCreateAccount);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,6 +52,67 @@ public class AccountFragment extends Fragment {
             }
         });
 
+        context = this.getActivity();
+
+        databaseUser = FirebaseDatabase.getInstance().getReference();
+
+        userList = new ArrayList<>();
+
+        anmeldenBtn = (Button) view.findViewById(R.id.buttonLogIn);
+        anmeldenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //databaseUser.addChildEventListener();
+                databaseUser.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        if (dataSnapshot.exists()) {
+                            for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                                User user = userSnapshot.getValue(User.class);
+                                userList.add(user);
+
+                                editTextInsertUsername = (EditText) view.findViewById(R.id.editTextInsertUsername);
+                                editTextInsertUsernameString = editTextInsertUsername.getText().toString().trim();
+
+                                if (user.getUsername() == editTextInsertUsernameString) {
+                                    Toast.makeText(context, "Anmeldung erfolgreich", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(context, "Username existiert nicht", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                /*databaseUser.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //aufgerufen, jedes Mal, wenn sich etwas in der DB ändert
+
+
+
+
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        //String tag;
+                        //Log.w(tag, "Failed to read value.", error.toException());
+                    }
+                });
+
+                 */
+            }
+        });
+
         return view;
     }
+
 }
--- app/src/main/java/com/example/navigationdrawerfromscratch/SearchFragment.java
+++ app/src/main/java/com/example/navigationdrawerfromscratch/SearchFragment.java
@@ -4,18 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class StartFragment extends Fragment {
+public class SearchFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_start, container, false);
+        return inflater.inflate(R.layout.fragment_search, container, false);
 
 
     }
